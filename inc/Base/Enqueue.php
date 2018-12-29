175f10a9b47d765fc44cc03948af0bd102c1bc20
Mario Kirolos <contact@mariofayek.com>
2018-12-29 12:29:26 -0500

    Updating the Enque File to add new CSS and JS files

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -44,14 +44,17 @@ use \MiamiTrips\Base\BaseController;
 	}
 
 	function frontendEnqueue(){
-		wp_enqueue_style('AutobodyStyles' , $this->plugin_url . 'assets/css/mainfrontend.css' );
-		wp_enqueue_style( 'bootstrap' , 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
-		wp_enqueue_style( 'style' , 'https://code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css');
+		wp_enqueue_style('miami-trips' , $this->plugin_url . 'assets/css/frontend.css' );
+		//Bootstrap
+		wp_enqueue_style('bootstrap' , $this->plugin_url . 'assets/css/bootstrap.min.css' );
+		
 	
-	
+		//Bootstrap 
+		wp_enqueue_script('bootstrap' , $this->plugin_url . 'assets/js/bootstrap.min.js' , __FILE__ );
+		
+		//Admin 
+		wp_enqueue_script('miami_trips' , $this->plugin_url . 'assets/js/frontend.js' , __FILE__ );
 
-		wp_enqueue_script('googleapis' , 'https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js');
-		wp_enqueue_script( 'googleajax'  , 'https://code.jquery.com/ui/1.12.0/jquery-ui.js');
 	}
 
  }
