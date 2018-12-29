c33fb7ea3746703086eaa5dcc0f944c924ac19c5
Mario Kirolos <contact@mariofayek.com>
2018-12-29 12:29:03 -0500

    Adding CSS and JS Files

---
+++ assets/js/frontend.js
@@ -0,0 +1,43 @@
+$('#days_slider').on('slide.bs.carousel', function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+$(document).on('click' , '.itinerary_day' , function(e){
+    e.preventDefault();
+    $this = $(this);
+    target_id = $this.data('target');
+
+
+
+    console.log(target_id);
+
+
+    $('#itinerary').slideUp('fast' , function(){
+        $('#itinerary .card').removeClass('active');
+        $("#itinerary").find(".card[data-id='" + target_id + "']").addClass('active');
+
+        $(this).slideDown('fast');
+    });
+});
+
+$(document).on('click' , '.book-now' , function(e){
+    e.preventDefault();
+    trip_date = $(this).data('date');
+    console.log(trip_date);
+});
